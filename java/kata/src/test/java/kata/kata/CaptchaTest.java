ca18189bc87126377ca3eaad74506079172048a3
Somkiat <somkiat.p@gmail.com>
2013-11-23 20:52:05 +0700

    Refactoring code test for duplicate code

--- java/kata/src/test/java/kata/kata/CaptchaTest.java
+++ java/kata/src/test/java/kata/kata/CaptchaTest.java
@@ -4,37 +4,35 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class CaptchaTest {
-	
+
+	MyCaptcha myCaptcha = new MyCaptcha();
+
 	@Test
 	public void whenOnePlusOneShouldSeeTwo() throws Exception {
-		MyCaptcha myCaptcha = new MyCaptcha();
 		int result = myCaptcha.process("1", "+", "1");
 		assertEquals(2, result);
-		
+
 		result = myCaptcha.process("one", "+", "1");
 		assertEquals(2, result);
 	}
-	
+
 	@Test
 	public void whenThreeMinusOneShouldSeeTwo() throws Exception {
-		MyCaptcha myCaptcha = new MyCaptcha();
 		int result = myCaptcha.process("3", "-", "1");
 		assertEquals(2, result);
-		
+
 		result = myCaptcha.process("three", "-", "1");
 		assertEquals(2, result);
 	}
-	
+
 	@Test
 	public void whenFiveMultiplyTwoShouldSeeTen() throws Exception {
-		MyCaptcha myCaptcha = new MyCaptcha();
 		int result = myCaptcha.process("5", "*", "2");
 		assertEquals(10, result);
-		
+
 		result = myCaptcha.process("five", "*", "two");
 		assertEquals(10, result);
 	}
-	
+
 }

7d076a427d184fb130f00ad88cb4430324990134
Somkiat <somkiat.p@gmail.com>
2013-11-23 20:43:14 +0700

    Refactoring code to move mapping input from string to number to HashMap

--- java/kata/src/main/java/kata/kata/MyCaptcha.java
+++ java/kata/src/main/java/kata/kata/MyCaptcha.java
@@ -1,21 +1,28 @@
 package kata.kata;
 
+import java.util.HashMap;
+
 public class MyCaptcha {
 
+	private HashMap<String, String> numberMap = new HashMap<String, String>();
+
+	public MyCaptcha() {
+		numberMap.put("one", "1");
+		numberMap.put("three", "3");
+	}
+
 	public int process(String left, String operator, String right) {
-		if (left.equals("one")) {
-			left = "1";
-		} else if( left.equals("three") ) {
-			left = "3";
+		if (numberMap.containsKey(left)) {
+			left = numberMap.get(left);
 		}
-		if (right.equals("one")) {
-			right = "1";
+		if (numberMap.containsKey(right)) {
+			left = numberMap.get(right);
 		}
 
 		int result = 0;
 		if (operator.equals("+")) {
 			result = Integer.parseInt(left) + Integer.parseInt(right);
-		} else if( operator.equals("-") ) {
+		} else if (operator.equals("-")) {
 			result = Integer.parseInt(left) - Integer.parseInt(right);
 		}
 		return result;
